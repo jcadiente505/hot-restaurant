58ad5be1f6803166bcd7e64b9a90511dad8e20f5
tylenick <tylerlenick@gmail.com>
2018-08-04 10:00:39 -0400

    api routes and arrays

---
+++ routing/api-routes.js
@@ -0,0 +1,33 @@
+// Create New object with a table - takes in JSON input
+app.post("/api/tables", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body-parser middleware
+    var newtable = req.body;
+  
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newtable.routeName = newtable.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newtable);
+  
+    tableList.push(newtable);
+  
+    res.json(newtable);
+});
+
+// Create New object on the wait list - takes in JSON input
+app.post("/api/waitlist", function(req, res) {
+    // req.body hosts is equal to the JSON post sent from the user
+    // This works because of our body-parser middleware
+    var newwaitlist = req.body;
+  
+    // Using a RegEx Pattern to remove spaces from newCharacter
+    // You can read more about RegEx Patterns later https://www.regexbuddy.com/regex.html
+    newwaitlist.routeName = newwaitlist.name.replace(/\s+/g, "").toLowerCase();
+  
+    console.log(newwaitlist);
+  
+    waitList.push(newwaitlist);
+  
+    res.json(newwaitlist);
+  });
